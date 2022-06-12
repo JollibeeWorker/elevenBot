fc24f6bb1c8c7536f47fd151d478a35fccd1a3ee
Vincent Candelario <vincent@vcandelario.com>
2022-06-11 22:54:28 -0700

    added mongodb database

--- elevenBot.js
+++ elevenBot.js
@@ -1,26 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const fetch = require('node-fetch');
+const MongoClient = require('mongodb').MongoClient;
 const { MessageEmbed } = require('discord.js');
 require("dotenv").config();
 
 var TOKEN = process.env.TOKEN;
+var URI = process.env.URI;
 
-//function playerMatch() {
-    //fetch('https://www.elevenvr.club/api/v1/accounts/710080/matches/latest')
-    //        .then(response => response.json())
-    //        .then(data => console.log(data.data))
-    //        .then(data => message.channel.send(data.data));
-//}
-
-async function playerMatch(playerID) {
-    const response = await fetch(`https://www.elevenvr.club/api/v1/accounts/${playerID}/matches/latest`, {});
-    const json = await response.json();
-    const string = JSON.stringify(json.data.id);
-    const stringFix = string.replaceAll('"', '');
-    console.log(stringFix);
-    return stringFix;
+async function connectToCluster(URI) {
+    let mongoClient;
+    try {
+        mongoClient = new MongoClient(URI);
+        console.log('connectiong to MongoDB cluster... ');
+        await mongoClient.connect();
+        console.log('Successfully connected to MongoDB!');
+        return mongoClient;
+    } catch (error) {
+        console.error('Connection to MongoDB failed!', error);
+        process.exit();
+    }
 }
+
+connectToCluster(URI);
 
 async function playerSearch(username) {
     const response = await fetch(`https://www.elevenvr.club/accounts/search/${username}`);
